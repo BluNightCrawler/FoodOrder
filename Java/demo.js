e601c725648e5dacb92b924a63d643e73439efa2
BluNightCrawler <97077024+BluNightCrawler@users.noreply.github.com>
2022-03-14 14:04:03 -0700

    Added
    the blocks now get the visbilty in and out
    Next step is now linking everything to put the text into the message.

---
+++ Java/demo.js
@@ -0,0 +1,16 @@
+alert();
+const Txtinput = document.getElementById("Textinput");
+const visible = document.getElementById("hidden");
+const delivery = document.getElementById("Deleivery");
+
+
+function myFunction(){
+    document.getElementById("hidden").style.visibility = "visible"
+}           
+
+function myFunction2(){
+    document.getElementById("hidden").style.visibility = "hidden"
+}
+function hidden(){
+    document.onload().style.visibility = "hidden"
+} 
